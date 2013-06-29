8e8345fe8db277e7d3f4cd86648eaae584c43716
prateeksachan <ps@prateeksachan.com>
2013-06-30 01:45:11 +0530

    MDL-31989: Minor indentation

--- blocks/globalsearch/globalsearch_html.php
+++ blocks/globalsearch/globalsearch_html.php
@@ -17,12 +17,12 @@ class globalsearch_form extends moodleform {
 
 		//-------------------------------------------------------
 		$mform->addElement('header', 'filterquerysection', get_string('filterqueryheader', 'search'));
-	    $mform->addElement('text', 'titlefilterqueryfield', get_string('titlefilterquery', 'search'));
+		$mform->addElement('text', 'titlefilterqueryfield', get_string('titlefilterquery', 'search'));
 		$mform->setType('titlefilterqueryfield', PARAM_TEXT);
-		
+
 		$mform->addElement('text', 'authorfilterqueryfield', get_string('authorfilterquery', 'search'));
 		$mform->setType('authorfilterqueryfield', PARAM_TEXT);
-		
+
 		$mods = search_get_modules();
 		$modules = array();
 		$modules [] = "All modules";
@@ -30,7 +30,7 @@ class globalsearch_form extends moodleform {
 			$modules[$mod->name] = ucfirst($mod->name);
 		}
 		$mform->addElement('select', 'modulefilterqueryfield', get_string('modulefilterquery', 'search'), $modules);
-		
+
 		$this->add_action_buttons($cancel = false, $submitlabel='Search');
 		$mform->setDefault('action', '');
 
