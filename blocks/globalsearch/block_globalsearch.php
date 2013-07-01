65302fb5a5baf5e3876d0b426e6c252652b77151
prateeksachan <ps@prateeksachan.com>
2013-07-01 13:42:02 +0530

    MDL-31989: Added indexing stats + cleanup

--- blocks/globalsearch/block_globalsearch.php
+++ blocks/globalsearch/block_globalsearch.php
@@ -12,52 +12,65 @@
 
 class block_globalsearch extends block_base {
 
-  function init() {
-     $this->title = get_string('pluginname', 'block_globalsearch');
-  }
+    function init() {
+        $this->title = get_string('pluginname', 'block_globalsearch');
+    }
 	
 	//this block has a settings.php file @TODO settings.php
 	function has_config() {
-    return true;
-  } 
+        return true;
+    } 
 
 	function get_content() {
 		global $CFG, $OUTPUT;
-    if ($this->content !== null) {
-  	 	return $this->content;
+        if ($this->content !== null) {
+  	 	   return $this->content;
+        }
+
+        require_once('globalsearch_html.php');
+        require_once($CFG->dirroot . '/search/lib.php');
+        $block_mform = new globalsearch_form();
+
+        $this->content         =  new stdClass;
+        $this->content->footer = '';
+
+        //getting the activated mods list
+        $mods = search_get_modules();
+        $modules = array();
+        $modules [] = "All modules";
+        foreach ($mods as $mod){
+            $modules[$mod->name] = ucfirst($mod->name);
+        }
+
+        $url = new moodle_url('/search/index.php');
+        $this->content->footer .= html_writer::link($url, get_string('advancequeries', 'search'));
+
+        $this->content->text  = html_writer::start_tag('div', array('class' => 'searchform'));
+        //$this->content->text .= $block_mform->display();
+        $this->content->text .= '<form action="'.$CFG->wwwroot.'/search/index.php" style="display:inline">';
+        $this->content->text .= '<fieldset class="invisiblefieldset">';
+        $this->content->text .= '<label for="searchform_search">Search: </label>'.
+                                '<input id="searchform_search" name="search" type="text" size="15" />';
+        $this->content->text .= $OUTPUT->help_icon('globalsearch', 'search');
+        $this->content->text .= '<label for="searchform_fq_module">Search in: </label><br>' . 
+                                '<select name="fq_module" id="searchform_fq_module">';
+        foreach ($modules as $key => $value){
+            $this->content->text .= '<option value="' . $key . '">' . $value . '</option>';
+        }
+        $this->content->text .= '</select>';
+        $this->content->text .= '<button id="searchform_button" type="submit" title="globalsearch">Go!</button><br />';
+        $this->content->text .= '</fieldset>';
+        $this->content->text .= '</form>';
+        $this->content->text .= html_writer::end_tag('div');
+
+        return $this->content;
     }
 
-    require_once('globalsearch_html.php');
-    $block_mform = new globalsearch_form();
-    
-    $this->content         =  new stdClass;
-    $this->content->footer = '';
+    //running the cron job for indexing
+    function cron(){
+        global $CFG;
 
-    $url = new moodle_url('/search/index.php');
-		$this->content->footer .= html_writer::link($url, get_string('advancequeries', 'search'));
-		
-		$this->content->text  = html_writer::start_tag('div', array('class' => 'searchform'));
-    //$this->content->text .= $block_mform->display();
-    $this->content->text .= '<form action="'.$CFG->wwwroot.'/search/index.php" style="display:inline">';
-    $this->content->text .= '<fieldset class="invisiblefieldset">';
-    $this->content->text .= '<label for="searchform_search">Search: </label>'.
-                            '<input id="searchform_search" name="search" type="text" size="15" /><br>';
-    $this->content->text .= '<label for="searchform_fq_module">Module Filter: </label>'.
-                            '<input id="searchform_fq_module" name="fq_module" type="text" size="15" />';
-    $this->content->text .= '<button id="searchform_button" type="submit" title="globalsearch">Go!</button><br />';
-    $this->content->text .= $OUTPUT->help_icon('globalsearch', 'search');
-    $this->content->text .= '</fieldset>';
-    $this->content->text .= '</form>';
-    $this->content->text .= html_writer::end_tag('div');
-
-    return $this->content;
-  }
-
-  	//running the cron job for indexing
-	function cron(){
-		global $CFG;
-
-    include($CFG->dirroot . '/search/cron.php');
-  }
+        include($CFG->dirroot . '/search/cron.php');
+    }
 
 }
