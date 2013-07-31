5c71de096ed19000503d3921556df18419fdc7a7
prateeksachan <ps@prateeksachan.com>
2013-07-31 22:30:26 +0530

    MDL-31989: Fixing exceptions while displaying results

--- search/search.php
+++ search/search.php
@@ -111,10 +111,12 @@ function solr_add_highlight_content($response) {
 
 function solr_merge_highlight_field_values($doc, $highlighteddoc) {
     $fields = array('user', 'author', 'name', 'title', 'intro');
-    if (empty($highlighteddoc->content)) {
-        $doc->content = substr($doc->content, 0, SEARCH_SET_FRAG_SIZE);
-    } else {
-        $doc->content = $highlighteddoc->content[0];
+    if (!empty($doc->content)) {
+        if (empty($highlighteddoc->content)) {
+            $doc->content = substr($doc->content, 0, SEARCH_SET_FRAG_SIZE);
+        } else {
+            $doc->content = $highlighteddoc->content[0];
+        }
     }
 
     foreach ($fields as $field) {
@@ -126,10 +128,10 @@ function solr_merge_highlight_field_values($doc, $highlighteddoc) {
                 break;
 
             default:
-            if(!empty($highlighteddoc->$field)) {
-                $doc->$field = reset($highlighteddoc->$field);
-            }
-            break;
+                if(!empty($highlighteddoc->$field)) {
+                    $doc->$field = reset($highlighteddoc->$field);
+                }
+                break;
         }
     }
 }
