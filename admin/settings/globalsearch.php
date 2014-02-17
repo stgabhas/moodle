b1e203a1b4b6405d97f17595561af8c7f43749fa
Eugene Venter <eugene@catalyst.net.nz>
2014-02-18 10:37:33 +1300

    admin/settings/globalsearch.php: "undefined var" fixes
    some notices that screwed caused troubles at installation time
    
    Change-Id: I881382cc6208ee0e3025abc2579ac63c727760dd

--- admin/settings/globalsearch.php
+++ admin/settings/globalsearch.php
@@ -15,22 +15,24 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
     $options = array('solr' => 'Apache Solr');
     $temp->add(new admin_setting_configselect('SEARCH_ENGINE', new lang_string('choosesearchengine', 'admin'), new lang_string('choosesearchengine_desc', 'admin'), 'solr', $options));
 
-    switch ($CFG->SEARCH_ENGINE) {
-        case 'solr':
-            if (function_exists('solr_get_version')) {
-                $version = solr_get_version();
-                $solr_installed = true;
-            }
-            break;
-        /*
-        case '{anothersearchengine}':
-            if (check_for_another_search_engine) {
-                {anothersearchengine}_installed = true;
-            }
-            break;
-        */
-        default:
-            break;
+    if (!empty($CFG->SEARCH_ENGINE)) {
+        switch ($CFG->SEARCH_ENGINE) {
+            case 'solr':
+                if (function_exists('solr_get_version')) {
+                    $version = solr_get_version();
+                    $solr_installed = true;
+                }
+                break;
+            /*
+            case '{anothersearchengine}':
+                if (check_for_another_search_engine) {
+                    {anothersearchengine}_installed = true;
+                }
+                break;
+            */
+            default:
+                break;
+        }
     }
 
     $ADMIN->add('globalsearch', $temp);
@@ -46,7 +48,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
         $temp->add(new admin_setting_configselect('SOLR_VERSION', new lang_string('solrversion', 'admin'), new lang_string('solrversion_desc', 'admin', $version), ($version == '1.0.3-alpha' ? '4.0' : '3.0'), $options));
         $temp->add(new admin_setting_configtext('SOLR_SERVER_HOSTNAME', new lang_string('solrserverhostname', 'admin'), new lang_string('solrserverhostname_desc', 'admin'), $hostname, PARAM_TEXT));
         $temp->add(new admin_setting_configcheckbox('SOLR_SECURE', new lang_string('solrsecuremode', 'admin'), new lang_string('solrsecuremode_desc', 'admin'), 0, 1, 0));
-        $temp->add(new admin_setting_configtext('SOLR_SERVER_PORT', new lang_string('solrhttpconnectionport', 'admin'), new lang_string('solrhttpconnectionport_desc', 'admin'), (($CFG->SOLR_SECURE) ? 8443 : 8983), PARAM_INT));
+        $temp->add(new admin_setting_configtext('SOLR_SERVER_PORT', new lang_string('solrhttpconnectionport', 'admin'), new lang_string('solrhttpconnectionport_desc', 'admin'), (!empty($CFG->SOLR_SECURE) ? 8443 : 8983), PARAM_INT));
         $temp->add(new admin_setting_configtext('SOLR_SERVER_USERNAME', new lang_string('solrauthuser', 'admin'), new lang_string('solrauthuser_desc', 'admin'), '', PARAM_RAW));
         $temp->add(new admin_setting_configtext('SOLR_SERVER_PASSWORD', new lang_string('solrauthpassword', 'admin'), new lang_string('solrauthpassword_desc', 'admin'), '', PARAM_RAW));
         $temp->add(new admin_setting_configtext('SOLR_SERVER_TIMEOUT', new lang_string('solrhttpconnectiontimeout', 'admin'), new lang_string('solrhttpconnectiontimeout_desc', 'admin'), 30, PARAM_INT));
@@ -68,7 +70,7 @@ if ($hassiteconfig) { // speedup for non-admins, add all caps used on this page
         $ADMIN->add('globalsearch', $temp);     
     */
 
-    if ($CFG->enableglobalsearch) {
+    if (!empty($CFG->enableglobalsearch)) {
         $temp = new admin_settingpage('activatemods', new lang_string('activatemods', 'admin'));
 
         $supported_mods = array('course', 'book', 'forum', 'glossary', 'label', 'lesson', 'page', 'resource', 'url', 'wiki'); // add a module here to make it gs_supported
