9bbb4d31cea316b78537a39166ed17e60adc8259
Eugene Venter <eugene@catalyst.net.nz>
2014-02-20 13:54:33 +1300

    course/lib.php: globalsearch - allow guest access
    Change-Id: Ib2e68694278ee0143fa655ebf73f25c894ca4028

--- course/lib.php
+++ course/lib.php
@@ -3763,7 +3763,7 @@ function course_search_access($id) {
     }
 
     $context = context_course::instance($course->id);
-    if (!is_enrolled($context) && !is_viewing($context)) {
+    if (!is_enrolled($context) && !is_viewing($context) && !is_guest($context)) {
         return SEARCH_ACCESS_DENIED;
     }
 
